4ff12853da2f5bce27046408bda5b1c3353af9a3
haydnv <haydnv@gmail.com>
2023-01-13 15:50:09 +0530

    remove PEM encoding and just use a hexadecimal string

--- python/rjwt/actor.py
+++ python/rjwt/actor.py
@@ -1,7 +1,7 @@
 import base64
 import json
 
-from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
+from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PublicKey, Ed25519PrivateKey
 from cryptography.hazmat.primitives import serialization
 
 from .token import Token
@@ -10,32 +10,42 @@ from .token import Token
 class Actor(object):
     def __init__(self, actor_id, public_key=None, private_key=None):
         if public_key:
-            self.public_key = public_key
+            self._public_key = Ed25519PublicKey.from_public_bytes(public_key)
 
             if private_key:
-                self.private_key = Ed25519PrivateKey.from_private_bytes(private_key)
+                self._private_key = Ed25519PrivateKey.from_private_bytes(private_key)
 
-                assert self.private_key.public_key() == self.public_key, f"{actor_id} has an invalid keypair"
+                if self._private_key.public_key() != self._public_key:
+                    raise ValueError(f"{actor_id} has an invalid keypair")
             else:
-                self.private_key = None
+                self._private_key = None
 
         elif private_key:
             raise ValueError(f"{actor_id} is missing a public key")
 
         else:
-            self.private_key = Ed25519PrivateKey.generate()
-            self.public_key = self.private_key.public_key()
+            self._private_key = Ed25519PrivateKey.generate()
+            self._public_key = self._private_key.public_key()
 
-        self.id = actor_id
+        self._id = actor_id
+
+    def id(self):
+        return self._id
+
+    def public_key(self):
+        return self._public_key.public_bytes(
+            encoding=serialization.Encoding.Raw,
+            format=serialization.PublicFormat.Raw
+        )
 
     def sign_token(self, token):
-        if not self.private_key:
+        if not self._private_key:
             raise RuntimeError(f"{self.actor_id} private key must be known in order to sign a message")
 
         headers = _base64_json_encode(token.headers())
         claims = _base64_json_encode(token.claims())
 
-        signature = self.private_key.sign(f"{headers}.{claims}".encode("ascii"))
+        signature = self._private_key.sign(f"{headers}.{claims}".encode("ascii"))
         signature = _base64_encode(signature)
 
         signed = f"{headers}.{claims}.{signature}"
@@ -47,7 +57,7 @@ class Actor(object):
 
         signature = _base64_decode(signature)
 
-        self.public_key.verify(signature, f"{headers}.{claims}".encode("ascii"))
+        self._public_key.verify(signature, f"{headers}.{claims}".encode("ascii"))
 
         headers = _base64_json_decode(headers)
         if headers != Token.headers():
@@ -56,25 +66,11 @@ class Actor(object):
         claims = _base64_json_decode(claims)
         token = Token.with_claims(**claims)
 
-        if token.actor_id == self.id:
+        if token.actor_id == self._id:
             return token
         else:
             raise ValueError("the token passed validation but has an unexpected actor ID " +
-                             f"{token.actor_id} (expected {self.id})")
-
-
-def public_pem_encode(public_key):
-    if not hasattr(public_key, "public_bytes"):
-        raise TypeError(f"expected a public key, not {public_key}")
-
-    return public_key.public_bytes(
-        format=serialization.PublicFormat.SubjectPublicKeyInfo,
-        encoding=serialization.Encoding.PEM,
-    )
-
-
-def public_pem_decode(pem_string):
-    return serialization.load_pem_public_key(pem_string)
+                             f"{token.actor_id} (expected {self._id})")
 
 
 def _base64_decode(ascii_string):
--- python/rjwt/tests/test_actor.py
+++ python/rjwt/tests/test_actor.py
@@ -2,33 +2,26 @@ import rjwt
 import unittest
 
 from cryptography.exceptions import InvalidSignature
-from cryptography.hazmat.primitives import serialization
 
 
 class TokenTests(unittest.TestCase):
     def testSignAndValidate(self):
         actor_one = rjwt.Actor("áctor १")
         actor_two = rjwt.Actor("áctor २")
-        token = rjwt.Token.issue(actor_one.id, "unit test", 30)
+        token = rjwt.Token.issue(actor_one.id(), "unit test", 30)
         signed = actor_one.sign_token(token)
 
         self.assertEqual(actor_one.verify(signed), token)
         self.assertRaises(InvalidSignature, lambda: actor_two.verify(signed))
 
-    def testPublicPEM(self):
-        actor = rjwt.Actor("actor")
-        encoded = rjwt.public_pem_encode(actor.public_key)
-        decoded = rjwt.public_pem_decode(encoded)
-        self.assertEqual(
-            decoded.public_bytes(
-                encoding=serialization.Encoding.Raw,
-                format=serialization.PublicFormat.Raw
-            ),
-            actor.public_key.public_bytes(
-                encoding=serialization.Encoding.Raw,
-                format=serialization.PublicFormat.Raw
-            )
-        )
+    def testPublicKey(self):
+        actor_one = rjwt.Actor("one")
+        encoded = actor_one.public_key().hex()
+
+        decoded = bytes.fromhex(encoded)
+        actor_two = rjwt.Actor("two", decoded)
+
+        self.assertEqual(actor_one.public_key(), actor_two.public_key())
 
 
 if __name__ == "__main__":
